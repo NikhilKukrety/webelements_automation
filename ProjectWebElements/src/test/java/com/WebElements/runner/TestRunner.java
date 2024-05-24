18f273a7ddab3870050af7229a14fc43d6749039
NikhilKukrety <kukretynikhil4@gmail.com>
2024-05-24 11:43:19 +0530

    included TestRunner

--- ProjectWebElements/src/test/java/com/WebElements/runner/TestRunner.java
+++ ProjectWebElements/src/test/java/com/WebElements/runner/TestRunner.java
@@ -1,5 +1,20 @@
 package com.WebElements.runner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features = "C:\\Users\\lenovo\\git\\repository\\ProjectWebElements\\src\\test\\java\\com\\WebElements\\features\\WebElements.feature",
+glue={"com.WebElements.stepDefinitions"},
+monochrome=true,
+plugin={"html:target/Cucumber-reports.html"}
+//tags = "@FullRegression"
+)
+
+
 public class TestRunner {
 
 }
