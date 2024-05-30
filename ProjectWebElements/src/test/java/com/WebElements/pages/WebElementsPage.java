9de3a253e1cf72d2160607b9a5ae926a8edacd31
NikhilKukrety <kukretynikhil4@gmail.com>
2024-05-31 00:30:58 +0530

    Merge branch 'main' of https://github.com/NikhilKukrety/WebElementsAutomation.git into main

--- ProjectWebElements/src/test/java/com/WebElements/pages/WebElementsPage.java
+++ ProjectWebElements/src/test/java/com/WebElements/pages/WebElementsPage.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
+//import org.testng.Assert;
 
 import com.WebElements.base.BaseClass;
 
